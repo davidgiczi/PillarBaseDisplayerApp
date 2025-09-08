0aee84aaf6d26d39ec66767e1d9da90a3dc2decd
david.giczi@gmail.com <david.giczi@gmail.com>
2025-09-08 19:57:21 +0200

    DebugForFindingPillarBasePoint

--- app/src/main/java/com/david/giczi/pillarbasedisplayerapp/MainActivity.java
+++ app/src/main/java/com/david/giczi/pillarbasedisplayerapp/MainActivity.java
@@ -241,11 +241,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                         eov.getCoordinatesForEOV().get(1)),
                 new Point("center", PILLAR_BASE_COORDINATES.get(FIND_POINT_INDEX).getX_coord(),
                        PILLAR_BASE_COORDINATES.get(FIND_POINT_INDEX).getY_coord()));
-        double direction = Math.toDegrees(pillarData.calcAzimuth()) + northPoleDirection > 360 ?
+        double direction = Math.toDegrees(pillarData.calcAzimuth()) + northPoleDirection >= 360 ?
                 Math.toDegrees(pillarData.calcAzimuth()) + northPoleDirection - 360 :
-                Math.toDegrees(pillarData.calcAzimuth()) + northPoleDirection;
+                Math.toDegrees(pillarData.calcAzimuth()) - northPoleDirection;
         addPillarDirectionArrowImage((float) direction, (int) Math.round(pillarData.calcDistance()) );
-        String pillarDirectionAndDistance = "Irány: "  + String.format(Locale.getDefault(),"%5.1f°", direction) +
+        String pillarDirectionAndDistance = "Irány: "  + String.format(Locale.getDefault(),"%5.1f°",
+                0 > direction ? direction + 360 : direction) +
           "\t\tTávolság: " + String.format(Locale.getDefault(),"%5.0fm", pillarData.calcDistance());
         TextView pillarDataView = gpsDataContainer.findViewById(R.id.pillar_direction_and_distance);
         pillarDataView.setText(pillarDirectionAndDistance);
