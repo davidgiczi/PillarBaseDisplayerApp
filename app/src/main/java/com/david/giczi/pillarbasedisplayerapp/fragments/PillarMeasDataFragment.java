cc854b09aeefea76a7f10d85509a7dba999cf118
david.giczi@gmail.com <david.giczi@gmail.com>
2025-12-19 11:55:59 +0100

    SetMeasPillarData

--- app/src/main/java/com/david/giczi/pillarbasedisplayerapp/fragments/PillarMeasDataFragment.java
+++ app/src/main/java/com/david/giczi/pillarbasedisplayerapp/fragments/PillarMeasDataFragment.java
@@ -14,23 +14,16 @@ import androidx.navigation.Navigation;
 import com.david.giczi.pillarbasedisplayerapp.MainActivity;
 import com.david.giczi.pillarbasedisplayerapp.R;
 import com.david.giczi.pillarbasedisplayerapp.databinding.FragmentMeasDataBinding;
-import com.david.giczi.pillarbasedisplayerapp.db.PillarBaseParamsService;
-import com.david.giczi.pillarbasedisplayerapp.service.AzimuthAndDistance;
 import com.david.giczi.pillarbasedisplayerapp.service.PillarLocationCalculator;
-import com.david.giczi.pillarbasedisplayerapp.service.Point;
-
-import java.util.Locale;
 
 public class PillarMeasDataFragment extends Fragment {
 
     private FragmentMeasDataBinding fragmentMeasDataBinding;
-    private PillarBaseParamsService service;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         this.fragmentMeasDataBinding = FragmentMeasDataBinding.inflate(inflater, container, false);
-        this.service = ((MainActivity) requireActivity()).service;
         MainActivity.PAGE_COUNTER = 1;
         MainActivity.MENU.findItem(R.id.start_stop_gps).setEnabled(false);
         MainActivity.MENU.findItem(R.id.save_pillar_center).setEnabled(false);
@@ -88,7 +81,7 @@ public class PillarMeasDataFragment extends Fragment {
                 Toast.makeText(getContext(), "Koordináták nem számíthatók", Toast.LENGTH_LONG).show();
                 return;
             }
-            MainActivity.MEAS_PILLAR_DATA = new String[16];
+            MainActivity.MEAS_PILLAR_DATA = new String[18];
             MainActivity.MEAS_PILLAR_DATA[0] = fragmentMeasDataBinding.centerFoot1YCoordinate.getText().toString();
             MainActivity.MEAS_PILLAR_DATA[1] = fragmentMeasDataBinding.centerFoot1XCoordinate.getText().toString();
             MainActivity.MEAS_PILLAR_DATA[2] = fragmentMeasDataBinding.centerFoot2YCoordinate.getText().toString();
@@ -105,6 +98,8 @@ public class PillarMeasDataFragment extends Fragment {
             MainActivity.MEAS_PILLAR_DATA[13] = fragmentMeasDataBinding.directionFoot3XCoordinate.getText().toString();
             MainActivity.MEAS_PILLAR_DATA[14] = fragmentMeasDataBinding.directionFoot4YCoordinate.getText().toString();
             MainActivity.MEAS_PILLAR_DATA[15] = fragmentMeasDataBinding.directionFoot4XCoordinate.getText().toString();
+            MainActivity.MEAS_PILLAR_DATA[16] = fragmentMeasDataBinding.abscissaDistanceOfNewPillar.getText().toString();
+            MainActivity.MEAS_PILLAR_DATA[17] = fragmentMeasDataBinding.ordinateDistanceOfNewPillar.getText().toString();
             Bundle resultData = new Bundle();
             resultData.putString("calcCenterX", MainActivity.calcPillarLocationData.centerX);
             resultData.putString("calcCenterY", MainActivity.calcPillarLocationData.centerY);
@@ -114,9 +109,7 @@ public class PillarMeasDataFragment extends Fragment {
             Navigation.findNavController(requireView())
                     .navigate(R.id.action_MeasDataFragment_to_DataFragment);
         });
-        if( service.actualPillarBase != null ){
-            setAbscissaAndOrdinateValue();
-        }
+
         return fragmentMeasDataBinding.getRoot();
     }
 
@@ -139,6 +132,8 @@ public class PillarMeasDataFragment extends Fragment {
             fragmentMeasDataBinding.directionFoot3XCoordinate.setText(MainActivity.MEAS_PILLAR_DATA[13]);
             fragmentMeasDataBinding.directionFoot4YCoordinate.setText(MainActivity.MEAS_PILLAR_DATA[14]);
             fragmentMeasDataBinding.directionFoot4XCoordinate.setText(MainActivity.MEAS_PILLAR_DATA[15]);
+            fragmentMeasDataBinding.abscissaDistanceOfNewPillar.setText(MainActivity.MEAS_PILLAR_DATA[16]);
+            fragmentMeasDataBinding.ordinateDistanceOfNewPillar.setText(MainActivity.MEAS_PILLAR_DATA[17]);
         }
     }
 
@@ -146,61 +141,6 @@ public class PillarMeasDataFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setMeasPillarData();
-    }
-
-    private void setAbscissaAndOrdinateValue(){
-        if( service.actualPillarBase.centerPillarX == null ||
-                service.actualPillarBase.centerPillarY == null ||
-                service.actualPillarBase.directionPillarX == null ||
-                service.actualPillarBase.directionPillarY == null ||
-                service.actualPillarBase.controlPointX == null ||
-                service.actualPillarBase.controlPointY == null ){
-            return;
-        }
-        Point controlPoint = new Point(service.actualPillarBase.controlPointId,
-                Double.parseDouble(service.actualPillarBase.controlPointY),
-                Double.parseDouble(service.actualPillarBase.controlPointX));
-        Point centerPoint = new Point(service.actualPillarBase.centerPillarId,
-                Double.parseDouble(service.actualPillarBase.centerPillarY),
-                Double.parseDouble(service.actualPillarBase.centerPillarX));
-        Point nextPoint = new Point(service.actualPillarBase.directionPillarId,
-                Double.parseDouble(service.actualPillarBase.directionPillarY),
-                Double.parseDouble(service.actualPillarBase.directionPillarX));
-        fragmentMeasDataBinding.abscissaDistanceOfNewPillar
-                        .setText(getAbscissaValue(controlPoint, centerPoint, nextPoint));
-        fragmentMeasDataBinding.ordinateDistanceOfNewPillar
-                .setText(getOrdinateValue(controlPoint, centerPoint, nextPoint));
-    }
-
-
-    private String getOrdinateValue(Point controlPoint, Point centerPoint, Point nextPoint){
-        if( controlPoint.equals(nextPoint) ){
-            return null;
-        }
-        else if( controlPoint.equals(centerPoint) ){
-            return "0.000";
-        }
-        double alfa = new AzimuthAndDistance(controlPoint, nextPoint).calcAzimuth() -
-                new AzimuthAndDistance(controlPoint, centerPoint).calcAzimuth();
-        double distance = new AzimuthAndDistance(controlPoint, centerPoint).calcDistance();
-
-        return String.format(Locale.getDefault(),
-                "%.3f", Math.sin(alfa) * distance).replace(",", ".");
-    }
-
-    private String getAbscissaValue(Point controlPoint, Point centerPoint, Point nextPoint) {
-        if( controlPoint.equals(nextPoint) ){
-            return null;
-        }
-        else if( controlPoint.equals(centerPoint) ){
-            return "0.000";
-        }
-        double alfa = new AzimuthAndDistance(controlPoint, nextPoint).calcAzimuth() -
-                new AzimuthAndDistance(controlPoint, centerPoint).calcAzimuth();
-        double distance = new AzimuthAndDistance(controlPoint, centerPoint).calcDistance();
-
-        return String.format(Locale.getDefault(), "" +
-                "%.3f", Math.cos(alfa) * distance).replace(",", ".");
     }
 
     @Override
